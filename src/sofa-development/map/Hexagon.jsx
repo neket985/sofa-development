77be7f06aa0b7d810dc8f7efc97a49b0776b7670
100Rubley <toxa_98@inbox.ru>
2021-06-06 16:02:13 +0300

    style: add bases and redraw px to em

--- src/sofa-development/map/Hexagon.jsx
+++ src/sofa-development/map/Hexagon.jsx
@@ -1,9 +1,8 @@
 import s from './Hexagon.module.css'
 
-// в ПРОПСЫ приходит булево значение isBase, на его основе нужно добываить класс
 const Hexagon = (props) => {
   return (
-    <div className={s.wrapper}>
+    <div className={s.wrapper + ' ' + (props.isBase ? s.blue : '')}>
       <div className={s.center1}></div>
       <div className={s.center2}></div>
       <div className={s.center3}></div>
